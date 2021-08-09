da7dd019891908a52e32ddcabc78a7d92a46004d
lxkeepcoding <lx3332551532@163.com>
2021-08-09 23:46:35 +0800

    niuke one

---
+++ test.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS   1
+//1.实践出真知
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "Practice makes perfect!\n";
+//	return 0;
+//}
+
+//2.我是大V
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "v   v\n";
+//	cout << " v v\n";
+//	cout << "  v\n";
+//	return 0;
+//}
+
+//3.有容乃大
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "The size of short is " << sizeof(short) << " bytes." << endl;
+//	cout << "The size of int is " << sizeof(int) << " bytes." << endl;
+//	cout << "The size of long is " << sizeof(long) << " bytes." << endl;
+//	cout << "The size of long long is " << sizeof(long long) << " bytes." << endl;
+//	return 0;
+//}
+
+//4.小飞机
+//#include<stdio.h>
+//int main()
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//	return 0;
+//}
+
